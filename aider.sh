b01bdf55fa9377202f247e7c6e1df0323ea7e4ff
macklinhrw <macklingaming@gmail.com>
2024-11-08 09:23:54 -0800

    feat: prep for a i

--- aider.sh
+++ aider.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-aider --model openrouter/anthropic/claude-3.5-sonnet --architect --editor-model openrouter/anthropic/claude-3.5-sonnet --yes-always --env aider/.env --auto-lint
+aider --model openrouter/anthropic/claude-3.5-sonnet --architect --editor-model openrouter/anthropic/claude-3.5-sonnet --yes-always \
+ --env aider/.env --auto-lint --lint-cmd "zig ast-check" --auto-test --test-cmd "zig build test" --cache-prompts
